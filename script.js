42746016f9b991353966e0629fcb8adb27adcbd8
Camilo <129242584+Torrescam@users.noreply.github.com>
2025-02-12 21:10:50 -0500

    feat: Enhance dynamic grid with better sizing and interaction
    - Improved `createGrid()` to support any grid size
    - Used `Math.floor()` to prevent gaps
    - Added mouseover effect for interactive drawing
    - Optimized rendering with `DocumentFragment`"

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function createGrid(rows, cols) {
+  const containerSize = 900;
+
   const gridContainer = document.querySelector(".container");
+
+  gridContainer.innerHTML = "";
+  gridContainer.style.width = `${containerSize}px`;
+  gridContainer.style.height = `${containerSize}px`;
+
+  let squareSize = Math.floor(containerSize / Math.max(rows, cols));
   let squares = document.createDocumentFragment();
 
   for (let i = 0; i < rows * cols; i++) {
     const square = document.createElement("div");
-  
     square.className = "square";
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#eae2b7";
+    });
     squares.appendChild(square);
   }
   gridContainer.appendChild(squares);
 }
-
-createGrid(16, 16)
+createGrid(100, 100);
